23f825505a2c29a7f93fd20cb4931a3dadbb66ae
danielufeli <danielufeli@yahoo.com>
2022-01-28 22:46:34 +0100

    Add Styling and refactor code

--- src/modules/getGames.js
+++ src/modules/getGames.js
@@ -1,30 +1,27 @@
-const getGames = () => {
-  const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/fxcnsYcJrkT4mY83CQhy/scores/';
+import { getGamesApi } from './api';
 
-  const fetchGames = async () => {
-    const res = await fetch(url, { method: 'GET' });
-    const result = await res.json();
-    return result;
-  };
+const getGames = async () => {
+  const url =
+    'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/fxcnsYcJrkT4mY83CQhy/scores/';
 
-  fetchGames().then((game) => {
-    const { result } = game;
-    const gamesElement = document.getElementById('games');
-    let gamesDisplay = '';
-    if (result.length > 0) {
-      result.map((g) => {
-        gamesDisplay += `
+  const fetchedGames = await getGamesApi(url);
+  const { result } = fetchedGames;
+  const gamesElement = document.getElementById('games');
+  let gamesDisplay = '';
+  if (result.length > 0) {
+    const sortedArray = result.sort((a, b) => a.user.localeCompare(b.user));
+    sortedArray.map((g) => {
+      gamesDisplay += `
         <tr>
         <td>${g.user}: ${g.score}</td>
         </tr>
         `;
-        return game;
-      });
-      gamesElement.innerHTML = gamesDisplay;
-    } else {
-      gamesElement.textContent = 'No Scores';
-    }
-  });
+      return g;
+    });
+    gamesElement.innerHTML = gamesDisplay;
+  } else {
+    gamesElement.textContent = 'No Scores';
+  }
 };
 
 export default getGames;
